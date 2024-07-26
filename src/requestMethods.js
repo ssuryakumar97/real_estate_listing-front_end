1e6a916434adf178b9a604c516c267e517f1a3d3
surya <ssuryakumar97@gmail.com>
2024-07-26 10:33:55 +0530

    url changed

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 
-const base_url = "http://localhost:5000/api"
+// const base_url = "http://localhost:5000/api"
+const base_url = "https://real-estate-listing-back-end.onrender.comhttps://real-estate-listing-back-end.onrender.com/api"
 
 const token = localStorage.getItem("persist:root") ==null ?"": JSON.parse(JSON.parse(localStorage?.getItem("persist:root"))?.currentUser)?.token
 console.log(token)
